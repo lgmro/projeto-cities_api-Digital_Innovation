5bee227837b01a4e16a8a3bf8c412a3a6525c001
lgmro <lucasgmmro@gmail.com>
2021-07-05 20:26:16 -0300

    Developed 'API Rest' to country consultation

--- src/main/java/com/github/lgmro/citiesapi/CountryResource.java
+++ src/main/java/com/github/lgmro/citiesapi/CountryResource.java
@@ -4,10 +4,14 @@ import com.github.lgmro.citiesapi.countries.Country;
 import com.github.lgmro.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.awt.print.Pageable;
+
+import java.util.Optional;
 
 
 @RestController
@@ -21,4 +25,16 @@ public class CountryResource {
     public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        }
+
 }
